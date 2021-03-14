fa302931fb805ba27fb019ee2777390ee1b991f6
ade-butt <ade-butt@student.42.fr>
2021-03-14 16:32:35 +0100

    tg

---
+++ rush03.c
@@ -0,0 +1,37 @@
+void	ft_putchar(char c);
+
+void	rectangle(int x, int y)
+{
+	int row;
+	int col;
+
+	row = 1;
+	while (row <= y)
+	{
+		col = 1;
+		while (col <= x)
+		{
+			if ((col == 1 && row == 1) || (col == 1 && row == y))
+				ft_putchar('A');
+			else if ((col == x && row == 1) || (col == x && row == y))
+				ft_putchar('C');
+			else if ((col > 1 && col < x) && (row == 1 || row == y))
+				ft_putchar('B');
+			else if ((row > 1 && row < y) && (col == 1 || col == x))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			col++;
+		}
+		ft_putchar('\n');
+		row++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	if ((x <= 0) || (y <= 0))
+		return ;
+	else
+		rectangle(x, y);
+}
